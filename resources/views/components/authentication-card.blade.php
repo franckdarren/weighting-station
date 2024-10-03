296823d1fcd26b51fec677508155d95533024a57
mando_christ <mandochrist13@gmail.com>
2024-10-03 23:18:18 +0100

    intégration login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,22 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+<div class="min-h-screen flex flex-col justify-between items-center pt-6 sm:pt-0 bg-[url('../../public/build/assets/frame_login.png')]">
+<div class="bg-white flex justify-around w-full">
+    <img src="build/assets/Logo_pc-tech.jpg" alt="" class="w-[70px] h-auto" >
+    <img src="build/assets/drapeau_gabon.jpeg" alt="" class="w-[70px] h-auto">
+</div>
+    <div class="w-full flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-white font-bold text-[38px] ">Bienvenu à vous</h1>
+        <p class="text-white mb-5 text-[16px]">Veuillez vous connecter pour continuer</p>
+        <!-- {{ $logo }}  -->
+        <img src="build/assets/truck.gif" alt="" class="w-[100px] h-auto">
+
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-[500px] mt-6 px-6 py-4  overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
+    </div>
+    <div class="bg-white flex justify-center items-center w-full">
+        <p class="text-[14px] my-5">Copy right  @ 2024 KFC Weighting Station</p>
+</div>
 </div>
