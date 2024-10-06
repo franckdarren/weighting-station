f59beef22110b66a70bb1ced69d1b780c981d30f
kevin-ar_e <ayak57085@gmail.com>
2024-10-06 02:04:00 +0100

    add link add_user to the dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -133,6 +133,10 @@
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
+                            <x-dropdown-link href="{{ route('register') }}">
+                                {{ __('Add new user') }}
+                            </x-dropdown-link>
+
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                             <x-dropdown-link href="{{ route('api-tokens.index') }}">
                                 {{ __('API Tokens') }}
