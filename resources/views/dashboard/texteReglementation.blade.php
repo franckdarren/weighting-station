8ff4fc064bff812e237cc3be6722467066fc7073
kevin-ar_e <ayak57085@gmail.com>
2024-10-10 13:25:04 +0100

    dynaque chart

--- resources/views/dashboard/texteReglementation.blade.php
+++ resources/views/dashboard/texteReglementation.blade.php
@@ -1,7 +1,7 @@
 <!-- Textes de reglementations -->
 <x-app-layout>
     <div class="px-[5%] my-10">
-        <h1 class="underline text-center text-2xl font-bold animate-pulse">
+        <h1 class="underline text-center text-2xl font-bold border border-gray-500">
             RAPPEL DE QUELQUES DISPOSITIONS REGLEMENTAIRES
         </h1>
 
