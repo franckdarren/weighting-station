3e6b7dc3be9894bf1902deaab7061e3fc8e07b55
mando_christ <mandochrist13@gmail.com>
2024-10-11 06:19:15 +0100

    fix git ignore

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -6,7 +6,7 @@
 
         <div class="mb-4 text-sm text-gray-600">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-        </div>
+        </div> 
 
         <x-validation-errors class="mb-4" />
 
